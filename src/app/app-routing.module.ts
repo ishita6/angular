e80a2f421198d22d54b56d7b50b0574de75e86a0
Ishita <ishitabatra678@gmail.com>
2022-06-15 15:50:27 +0530

    added login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,17 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { jobComponent } from './pages/job/job.component';
 import { jobpageComponent } from './pages/jobpage/jobpage.component';
+import { loginComponent } from './pages/login/login.component';
 import { scholarshipComponent } from './pages/scholarship/scholarship.component';
 import { scholarshipPageComponent } from './pages/scholarshipPage/scholarshipPage.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
+  {path:'login',component:loginComponent},
+  {path:'sign-up',component:SignUpComponent},
+
+
   {path:'job',component:jobComponent},
   {path:'intern',component:jobComponent},
   {path:'admission',component:jobComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,27 @@ import { jobpageComponent } from './pages/jobpage/jobpage.component';
 
 import { scholarshipPageComponent } from './pages/scholarshipPage/scholarshipPage.component';
 import { scholarshipComponent } from './pages/scholarship/scholarship.component';
+import{ loginComponent} from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+// import { environment } from '../environments/environment';
+// import { provideAuth, getAuth } from '@angular/fire/auth';
+// import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+// import { HotToastModule } from '@ngneat/hot-toast';
+// import { LandingComponent } from './components/landing/landing.component';
+import { MatMenuModule } from '@angular/material/menu';
+// import { ProfileComponent } from './components/profile/profile.component';
+// import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -22,17 +44,37 @@ import { scholarshipComponent } from './pages/scholarship/scholarship.component'
     FooterComponent,
     HomeComponent,
     jobComponent,
+    loginComponent,
+    SignUpComponent,
 
     scholarshipComponent,
     scholarshipPageComponent,
     
     AboutComponent,
     ContactComponent,
-    jobpageComponent
+    jobpageComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    // MatFormFieldModule
+
+ 
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // provideAuth(() => getAuth()),
+    // provideFirestore(() => getFirestore()),
+    // provideStorage(() => getStorage()),
+    // HotToastModule.forRoot(),
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
