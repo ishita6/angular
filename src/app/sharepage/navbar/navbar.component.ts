609881a1c12895a2499bddff14242708307113ac
Ishita <ishitabatra678@gmail.com>
2022-06-25 19:45:03 +0530

    header

--- src/app/sharepage/navbar/navbar.component.ts
+++ src/app/sharepage/navbar/navbar.component.ts
@@ -1,33 +1,33 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-navbar',
-//   templateUrl: './navbar.component.html',
-//   styleUrls: ['./navbar.component.css']
-// })
-// export class NavbarComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
-
-import { Component } from '@angular/core';
 @Component({
-    // tslint:disable-next-line:component-selector
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
+export class NavbarComponent implements OnInit {
 
+  constructor() { }
 
-export class NavbarComponent {
-    public items: { field: string }[] = [
-        { field: 'Option 1' },
-        { field: 'Option 2' },
-        { field: 'Option 3' }
-    ];
+  ngOnInit(): void {
+  }
+
 }
+
+// import { Component } from '@angular/core';
+// @Component({
+//     // tslint:disable-next-line:component-selector
+//     selector: 'app-navbar',
+//     templateUrl: './navbar.component.html',
+//     styleUrls: ['./navbar.component.scss']
+// })
+
+
+// export class NavbarComponent {
+//     public items: { field: string }[] = [
+//         { field: 'Option 1' },
+//         { field: 'Option 2' },
+//         { field: 'Option 3' }
+//     ];
+// }
 
